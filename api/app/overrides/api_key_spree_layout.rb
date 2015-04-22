6fd98eecea4cc6b8f5de79454f714c8d25bf9561
Isaac Freeman <isaac@resolvedigital.co.nz>
2015-04-22 13:57:52 +1200

    Removes deface override for API key, as it was behaving weirdly for MWS

--- api/app/overrides/api_key_spree_layout.rb
+++
@@ -1,7 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
-                     :name => "api_key_spree_layout",
-                     :insert_bottom => "body",
-                     :partial => "spree/api/key",
-                     :disabled => false,
-                     :original => "eb4b04993e8e4d1c20a7c3d974dfed20b59aec4c")
-
